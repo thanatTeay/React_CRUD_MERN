0a8cb83868690c26154c2ec746af9628ebeca084
Thanat Jumneanbun <thanat.jumn@gmail.com>
2021-08-18 12:04:31 +0900

    deploy

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === 'production')
 {
-    app.use(express.static(path.join(__dirname,'./client/build')))
+    app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname, './client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
     })
 }
 
